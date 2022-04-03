88c458e0d3bb319750abbc9676df1423d3654863
pavel <malyshev21.03@gmail.com>
2022-04-03 18:10:30 +0700

    v.0.1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
+//Список 4 подгруппы:
+//A) Кравчук Б.Д.
+//B) Кремзер А.Д.
+//C) Малышев П.А.
+//D) Щербинина Д.В.
+
 #include <iostream>
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
