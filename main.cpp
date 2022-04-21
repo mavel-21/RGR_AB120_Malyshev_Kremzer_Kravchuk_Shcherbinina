86e8135d8ef8167758fd7203901595f5bd7411e2
krmz <75171579+imkrmz@users.noreply.github.com>
2022-04-21 20:06:42 +0700

    Update main.cpp
    Атбаш 1.0. Файловый ввод/вывод будет добавлен позже

--- main.cpp
+++ main.cpp
@@ -5,10 +5,50 @@
 //D) Щербинина Д.В.
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    // проверка работы
-    return 0;
+// АТБАШ 1.0. ФАЙЛОВЫЙ ВВОД/ВЫВОД БУДЕТ ДОБАВЛЕН ПОЗЖЕ
+
+string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string cab = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
+void atbash(string line, int choice)
+{
+    int strSize = line.size(), ind = 0;
+    for (int i = 0; i < strSize; i++)
+    {
+        if (isalpha(line[i]))
+        {
+            if (isupper(line[i]))
+            {
+                ind = int(line[i]) - 65;
+                if (choice) line[i] = cab[ind];
+                else line[i] = abc[ind];
+            }
+            else
+            {
+                ind = int(line[i]) - 97;
+                if (choice) line[i] = tolower(cab[ind]);
+                else line[i] = tolower(abc[ind]);
+            }
+        }
+    }
+    for (char elem : line)
+    {
+        cout << elem;
+    }
 }
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string line;
+    int choice;
+    cout << "Введите строку: ";
+    getline(cin, line);
+    cout << "Выберите, что требуется сделать со строкой:\n1) Зашифровать \t 2) Дешифровать" << endl;
+    cin >> choice;
+    atbash(line, choice);
+}
+
