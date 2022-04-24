40898d45c1fb13452cc72eae5c63065960b1b54c
krmz <75171579+imkrmz@users.noreply.github.com>
2022-04-24 21:44:29 +0700

    Update main.cpp
    Добавлен шифр с кодовым словом

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-// АТБАШ 1.1. ДОБАВЛЕН ФАЙЛОВЫЙ ВВОД-ВЫВОД И ВЫБОР ШИФРОВКИ
+// ДОБАВЛЕН ШИФР С КОДОВЫМ СЛОВОМ
 
 string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 string cab = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 ofstream outfile;
 ifstream infile;
 
-void atbash(string line, int choice)
+void atbash(string line, int mode)
 {
     int strSize = line.size(), ind = 0;
     for (int i = 0; i < strSize; i++)
@@ -26,13 +27,13 @@ void atbash(string line, int choice)
             if (isupper(line[i]))
             {
                 ind = int(line[i]) - 65;
-                if (choice) line[i] = cab[ind];
+                if (mode == 1) line[i] = cab[ind];
                 else line[i] = abc[ind];
             }
             else
             {
                 ind = int(line[i]) - 97;
-                if (choice) line[i] = tolower(cab[ind]);
+                if (mode == 1) line[i] = tolower(cab[ind]);
                 else line[i] = tolower(abc[ind]);
             }
         }
@@ -44,25 +45,73 @@ void atbash(string line, int choice)
     outfile << " " << endl;
 }
 
+void keywordABC(string line, string keyword, int mode) 
+{
+    vector <char> newABC;
+    int ind = 0;
+    for (size_t i = 0; i < keyword.size(); i++)
+    {
+        newABC.push_back(toupper(keyword[i]));
+    }
+    for (char elem : abc)
+    {
+        if (find(newABC.begin(), newABC.end(), elem) != newABC.end())
+            continue;
+        else
+        {
+            newABC.push_back(elem);
+        }
+    }
+    for (size_t i = 0; i < line.size(); i++)
+    {
+        if (isalpha(line[i]))
+        {
+            if (mode == 1)
+            {
+                ind = int(line[i]) - 65;
+                if (isupper(line[i])) line[i] = newABC[ind];
+                else line[i] = tolower(newABC[ind]);
+            }
+            else
+            {
+                auto indIt = find(newABC.begin(), newABC.end(), line[i]);
+                ind = indIt - newABC.begin();
+                if (isupper(line[i])) line[i] = abc[ind];
+                else line[i] = tolower(abc[ind]);
+            }
+        }
+    }
+    for (char elem : line)
+    {
+        outfile << elem;
+    }
+    outfile << " " << endl;
+}
 int main()
 {
     outfile.open("output.txt");
     infile.open("input.txt");
     setlocale(LC_ALL, "Russian");
-    string line;
-    int choice, algo;
+    string line, keyword;
+    int mode, algo;
     cout << "Выберите шифровку:\n1) Атбаш\t2) Шифр с использованием кодового слова\t       3) Табличная шифровка с ключевым словом" << endl;
     cin >> algo;
+    if ((algo == 2) || (algo == 3))
+    {
+        cout << "Введите кодовое слово: ";
+        cin >> keyword; // НЕ ЗАБЫТЬ: проверка на то, что в кодовом слове нет одинаковых букв
+    }
     cout << "Выберите, что требуется сделать с текстом:\n1) Зашифровать \t 2) Дешифровать" << endl;
-    cin >> choice;
+    cin >> mode;
     while (getline(infile, line))
     {
         switch (algo)
         {
         case 1:
-            atbash(line, choice);
+            atbash(line, mode);
             break;
         case 2:
+            keywordABC(line, keyword, mode);
             break;
         case 3:
             break;
