2412203cc8578558166229c7dee63cb01d15a42a
AKremzer <75171579+AKremzer@users.noreply.github.com>
2022-05-08 13:51:16 +0700

    Update main.cpp
    Добавлена проверка повторяющихся букв в кодовом слове

--- main.cpp
+++ main.cpp
@@ -13,29 +13,38 @@
 #include <vector>
 using namespace std;
 
-// ДОБАВЛЕН ШИФР С КОДОВЫМ СЛОВОМ
-
-string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // латинский алфавит
-string cab = "ZYXWVUTSRQPONMLKJIHGFEDCBA"; // латинский алфавит (зеркальная версия для шифра Атбаш)
-ofstream outfile; // файл вывода
-ifstream infile; // файл ввода
-
-void atbash(string line, int mode) // шифр Атбаш
+string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string cab = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+ofstream outfile;
+ifstream infile;
+int sameLetters(string word)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
+        for (int j = 0; j < word.length(); j++)
+        {
+            if (i != j && word[i] == word[j])
+                return 1;
+        }
+    }
+    return 0;
+}
+void atbash(string line, int mode)
 {
     int strSize = line.size(), ind = 0;
     for (int i = 0; i < strSize; i++)
     {
-        if (isalpha(line[i])) // проверка, является ли символ буквой
+        if (isalpha(line[i]))
         {
             if (isupper(line[i]))
             {
-                ind = int(line[i]) - 65; // получение номера буквы в алфавите (ее индекса в строке abc или cab)
-                if (mode == 1) line[i] = cab[ind]; // замена буквы на нужную букву из алфавита шифрования или дешифрования в зависимости от метода работы
+                ind = int(line[i]) - 65;
+                if (mode == 1) line[i] = cab[ind];
                 else line[i] = abc[ind];
             }
             else
             {
-                ind = int(line[i]) - 97; // аналогичный алгоритм для строчных букв
+                ind = int(line[i]) - 97;
                 if (mode == 1) line[i] = tolower(cab[ind]);
                 else line[i] = tolower(abc[ind]);
             }
@@ -43,50 +52,50 @@ void atbash(string line, int mode) // шифр Атбаш
     }
     for (char elem : line)
     {
-        outfile << elem; // вывод в файл
+        outfile << elem;
     }
     outfile << " " << endl;
 }
 
-void keywordABC(string line, string keyword, int mode) // шифр с использованием кодового слова
+void keywordABC(string line, string keyword, int mode) 
 {
     vector <char> newABC;
     int ind = 0;
     for (size_t i = 0; i < keyword.size(); i++)
     {
-        newABC.push_back(toupper(keyword[i])); // запись кодового слова в алфавит шифрования
+        newABC.push_back(toupper(keyword[i]));
     }
     for (char elem : abc)
     {
-        if (find(newABC.begin(), newABC.end(), elem) != newABC.end()) // проверка, является ли буква алфавита буквой кодового слова
+        if (find(newABC.begin(), newABC.end(), elem) != newABC.end())
             continue;
         else
         {
-            newABC.push_back(elem); // запись остального алфавита в алфавит шифрования
+            newABC.push_back(elem);
         }
     }
     for (size_t i = 0; i < line.size(); i++)
     {
-        if (isalpha(line[i])) // проверка, является ли символ буквой
+        if (isalpha(line[i]))
         {
             if (mode == 1)
             {
-                ind = int(line[i]) - 65; // получение номера буквы в алфавите (ее индекса в алфавите шифрования)
-                if (isupper(line[i])) line[i] = newABC[ind]; // замена буквы на букву с таким же индексом в алфавите шифрования
+                ind = int(line[i]) - 65;
+                if (isupper(line[i])) line[i] = newABC[ind];
                 else line[i] = tolower(newABC[ind]);
             }
             else
             {
-                auto indIt = find(newABC.begin(), newABC.end(), line[i]); // поиск индекса зашифрованного символа в словаре шифрования
+                auto indIt = find(newABC.begin(), newABC.end(), line[i]);
                 ind = indIt - newABC.begin();
-                if (isupper(line[i])) line[i] = abc[ind]; // замена буквы на букву с найденным индексом в латинском алфавите
+                if (isupper(line[i])) line[i] = abc[ind];
                 else line[i] = tolower(abc[ind]);
             }
         }
     }
     for (char elem : line)
     {
-        outfile << elem; // вывод в файл
+        outfile << elem;
     }
     outfile << " " << endl;
 }
@@ -98,15 +107,20 @@ int main()
     string line, keyword;
     int mode, algo;
     cout << "Выберите шифровку:\n1) Атбаш\t2) Шифр с использованием кодового слова\t       3) Табличная шифровка с ключевым словом" << endl;
-    cin >> algo; // выбор алгоритма шифрования
-    if ((algo == 2) || (algo == 3)) // введение кодового слова для некоторых алгоритмов
+    cin >> algo;
+    if ((algo == 2) || (algo == 3))
     {
-        cout << "Введите кодовое слово: ";
-        cin >> keyword; // НЕ ЗАБЫТЬ: проверка на то, что в кодовом слове нет одинаковых букв
+        do
+        {
+            cout << "Введите кодовое слово без повторяющихся букв: ";
+            cin >> keyword;
+            if (sameLetters(keyword) != 0)
+                cout << "В слове есть повторяющиеся буквы. ";
+        } while (sameLetters(keyword) != 0);
     }
     cout << "Выберите, что требуется сделать с текстом:\n1) Зашифровать \t 2) Дешифровать" << endl;
-    cin >> mode; // выбор режима работы (шифрование/дешифрование)
-    while (getline(infile, line)) // считывание текста построчно
+    cin >> mode;
+    while (getline(infile, line))
     {
         switch (algo)
         {
