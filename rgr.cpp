361951a4d5e901399dfba96bd396951a1524935c
AKremzer <75171579+AKremzer@users.noreply.github.com>
2022-06-05 17:21:02 +0700

    Update rgr.cpp

--- rgr.cpp
+++ rgr.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -391,6 +391,373 @@ void antitableCypher()
     file.close();
 }
 
+void Caesar() {            
+    int n;
+    while (true) {
+        cout << "Enter key: ";
+        cin >> n;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+            cout << "The key must be integer " << endl;
+        }
+        else if (n > 0) {
+
+            cin.ignore(INT_MAX, '\n');
+            break;
+        }
+        else {
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
+            cout << "The key must be natural " << endl;
+        }
+    }
+    string str = getString("Phrase: ", 8);
+    inFile("Key: " + to_string(n));
+    n = n % 26;
+    for (auto& c : str)
+    {
+        if (c <= 'a' && c >= 'z' || c <= 'A' && c >= 'Z') {
+            continue;
+        }
+        if (c >= 'a' && c <= 'z')
+            c = ((c - 'a' + n) % 26) + 'a';
+        else if (c >= 'A' && c <= 'Z')
+            c = ((c - 'A' + n) % 26) + 'A';
+    }
+    inFile("Caesar: " + str);
+}
+
+void antiCaesar() {
+    int n = stoi(getString("Key: ", 5));
+    string str = getString("Caesar: ", 8);
+    n = n % 26;
+    for (auto& c : str)
+    {
+        if (c <= 'a' && c >= 'z' || c <= 'A' && c >= 'Z') {
+            continue;
+        }
+        if (c >= 'a' && c <= 'z') {
+            if (c - n < 'a') {
+                c = c - n + 'z' - 'a' + 1;
+            }
+            else
+                c = ((c - 'a' - n) % 26) + 'a';
+        }
+        else if (c >= 'A' && c <= 'Z') {
+            if (c - n < 'A') {
+                c = c - n + 'Z' - 'A' + 1;
+            }
+            else
+                c = ((c - 'A' - n) % 26) + 'A';
+        }
+    }
+    inFile("Caesar decryption: " + str);
+}
+
+void DoubleTable() {            
+    string st, kl, kk, kl2, kk2, stOut = "";
+    char x;
+    int n, m, k, flag = 0;;
+    k = 0;
+    st = getString("Phrase: ", 8);
+    if (size(st) % 2 != 0)
+    {
+        cout << "   [ERROR] Can't use Double table permutation with a text of uneven length! Length of text is: " << size(st) << endl << endl;
+        return;
+    }
+    while (flag == 0)
+    {
+        cout << "Enter first key: ";
+        cin >> kl;
+        cout << "Enter second key: ";
+        cin >> kl2;
+        kk = kl;
+        kk2 = kl2;
+        n = size(kl);
+        m = size(kl2);
+        try {
+            flag = 1;
+            if (m * n != size(st))
+            {
+                flag = 0;
+                throw 1;
+            }
+            for (char elem : kl)
+            {
+                if (!isdigit(elem)) throw 1;
+                else
+                {
+                    int check = elem - '0';
+                    if (check > size(kl))
+                    {
+                        flag = 0;
+                        throw 1;
+                    }
+                }
+            }
+            for (char elem : kl2)
+            {
+                if (!isdigit(elem))
+                {
+                    flag = 0;
+                    throw 1;
+                }
+                else
+                {
+                    int check = elem - '0';
+                    if (check > size(kl2))
+                    {
+                        flag = 0;
+                        throw 1;
+                    }
+                }
+            }
+        }
+        catch (...) {
+            cout << "Incorrect keys! Note the rules:\n1) Length of key 1 * length of key 2 should be equal to text length!\n2) Key must contain only numbers!\n3)Digits of key shouldn't be more than length of key!" << endl;
+            cout << "Example: Text length = 12, Key 1 = 1324, Key 2 = 132" << endl;
+            cout << "Text length: " << size(st) << endl << endl;
+        }
+    }
+    inFile("Key 1: " + kl);
+    inFile("Key 2: " + kl2);
+    vector <vector <char>> v(m, vector <char>(n));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            v[i][j] = st[k];
+            k++;
+        }
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (kk[i] > kk[j]) {
+                x = kk[i];
+                kk[i] = kk[j];
+                kk[j] = x;
+                for (int k = 0; k < m; k++) {
+                    x = v[k][i];
+                    v[k][i] = v[k][j];
+                    v[k][j] = x;
+
+                }
+            }
+        }
+    }
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (kk2[i] > kk2[j]) {
+                x = kk2[i];
+                kk2[i] = kk2[j];
+                kk2[j] = x;
+                for (int k = 0; k < n; k++) {
+                    x = v[i][k];
+                    v[i][k] = v[j][k];
+                    v[j][k] = x;
+
+                }
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            stOut += v[i][j];
+
+        }
+
+    }
+    inFile("DoubleTable: " + stOut);
+}
+
+void antiDoubleTable() {
+    char x;
+    int n, m, k;
+    string stOut = "";
+    string st = getString("DoubleTable: ", 13);
+    string kl = getString("Key 1: ", 7);
+    string kl2 = getString("Key 2: ", 7);
+    string kk = kl;
+    string kk2 = kl2;
+    n = size(kl);
+    m = size(kl2);
+    vector <vector <char>> v(m, vector <char>(n));
+    vector <vector <char>> vt(m, vector <char>(n));
+    vector <vector <char>> vtt(m, vector <char>(n));
+    k = 0;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            v[i][j] = st[k];
+            k++;
+        }
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (kk[i] > kk[j]) {
+                x = kk[i];
+                kk[i] = kk[j];
+                kk[j] = x;
+            }
+        }
+    }
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (kk2[i] > kk2[j]) {
+                x = kk2[i];
+                kk2[i] = kk2[j];
+                kk2[j] = x;
+            }
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (kk2[j] == kl2[i]) {
+                for (int k = 0; k < n; k++) {
+                    vt[i][k] = v[j][k];
+                }
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (kk[j] == kl[i]) {
+                for (int k = 0; k < m; k++) {
+                    vtt[k][i] = vt[k][j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            stOut += vtt[i][j];
+        }
+    }
+    inFile("DoubleTable decryption: " + stOut);
+}
+
+void Tarab() {          //tarabarskaya gramota 
+    string word = getString("Phrase: ", 8);
+    int k = size(word);
+    string stOut = "";
+    for (int i = 0; i < k; i++) {
+        if (word[i] <= 'a' && word[i] >= 'z' || word[i] <= 'A' && word[i] >= 'Z') {
+            continue;
+        }
+        if (word[i] >= 'a' && word[i] <= 'z')
+            switch (word[i]) {
+            case 'b': word[i] = 'n'; break;
+            case 'c': word[i] = 'p'; break;
+            case 'd': word[i] = 'q'; break;
+            case 'f': word[i] = 'r'; break;
+            case 'g': word[i] = 's'; break;
+            case 'h': word[i] = 't'; break;
+            case 'j': word[i] = 'v'; break;
+            case 'k': word[i] = 'w'; break;
+            case 'l': word[i] = 'x'; break;
+            case 'm': word[i] = 'z'; break;
+
+            case 'n': word[i] = 'b'; break;
+            case 'p': word[i] = 'c'; break;
+            case 'q': word[i] = 'd'; break;
+            case 'r': word[i] = 'f'; break;
+            case 's': word[i] = 'g'; break;
+            case 't': word[i] = 'h'; break;
+            case 'v': word[i] = 'j'; break;
+            case 'w': word[i] = 'k'; break;
+            case 'x': word[i] = 'l'; break;
+            case 'z': word[i] = 'm'; break;
+            }
+        else
+            switch (word[i]) {
+            case 'B': word[i] = 'N'; break;
+            case 'C': word[i] = 'P'; break;
+            case 'D': word[i] = 'Q'; break;
+            case 'F': word[i] = 'R'; break;
+            case 'G': word[i] = 'S'; break;
+            case 'H': word[i] = 'T'; break;
+            case 'J': word[i] = 'V'; break;
+            case 'K': word[i] = 'W'; break;
+            case 'L': word[i] = 'X'; break;
+            case 'M': word[i] = 'Z'; break;
+
+            case 'N': word[i] = 'B'; break;
+            case 'P': word[i] = 'C'; break;
+            case 'Q': word[i] = 'D'; break;
+            case 'R': word[i] = 'F'; break;
+            case 'S': word[i] = 'G'; break;
+            case 'T': word[i] = 'H'; break;
+            case 'V': word[i] = 'J'; break;
+            case 'W': word[i] = 'K'; break;
+            case 'X': word[i] = 'L'; break;
+            case 'Z': word[i] = 'M'; break;
+            }
+        stOut += word[i];
+    }
+    inFile("Tarabarskaya: " + stOut);
+}
+
+void antiTarab() {           
+    string word = getString("Tarabarskaya: ", 14);
+    int k = size(word);
+    string stOut = "";
+    for (int i = 0; i < k; i++) {
+        if (word[i] <= 'a' && word[i] >= 'z' || word[i] <= 'A' && word[i] >= 'Z') {
+            continue;
+        }
+        if (word[i] >= 'a' && word[i] <= 'z')
+            switch (word[i]) {
+            case 'b': word[i] = 'n'; break;
+            case 'c': word[i] = 'p'; break;
+            case 'd': word[i] = 'q'; break;
+            case 'f': word[i] = 'r'; break;
+            case 'g': word[i] = 's'; break;
+            case 'h': word[i] = 't'; break;
+            case 'j': word[i] = 'v'; break;
+            case 'k': word[i] = 'w'; break;
+            case 'l': word[i] = 'x'; break;
+            case 'm': word[i] = 'z'; break;
+
+            case 'n': word[i] = 'b'; break;
+            case 'p': word[i] = 'c'; break;
+            case 'q': word[i] = 'd'; break;
+            case 'r': word[i] = 'f'; break;
+            case 's': word[i] = 'g'; break;
+            case 't': word[i] = 'h'; break;
+            case 'v': word[i] = 'j'; break;
+            case 'w': word[i] = 'k'; break;
+            case 'x': word[i] = 'l'; break;
+            case 'z': word[i] = 'm'; break;
+            }
+        else
+            switch (word[i]) {
+            case 'B': word[i] = 'N'; break;
+            case 'C': word[i] = 'P'; break;
+            case 'D': word[i] = 'Q'; break;
+            case 'F': word[i] = 'R'; break;
+            case 'G': word[i] = 'S'; break;
+            case 'H': word[i] = 'T'; break;
+            case 'J': word[i] = 'V'; break;
+            case 'K': word[i] = 'W'; break;
+            case 'L': word[i] = 'X'; break;
+            case 'M': word[i] = 'Z'; break;
+
+            case 'N': word[i] = 'B'; break;
+            case 'P': word[i] = 'C'; break;
+            case 'Q': word[i] = 'D'; break;
+            case 'R': word[i] = 'F'; break;
+            case 'S': word[i] = 'G'; break;
+            case 'T': word[i] = 'H'; break;
+            case 'V': word[i] = 'J'; break;
+            case 'W': word[i] = 'K'; break;
+            case 'X': word[i] = 'L'; break;
+            case 'Z': word[i] = 'M'; break;
+            }
+        stOut += word[i];
+    }
+    inFile("Tarabarskaya decryption: " + stOut);
+}
+
 int main() {
     string dialogStr; // диалоговая строка
     string pinAdmin = "1111"; // дефолтный пинкод
@@ -425,9 +792,9 @@ int main() {
             string cipher_str;
             int cipher;
             cout
-                << "\t1. Gronsfeld cipher\n\t2. Encryption using the square of Polybius\n\t3. Simple table permutation\n\t4. Atbash cipher\n\t5. Keyword cipher\n\t6. Table cipher with a keyword\n\tAny other character: go back\nChoose encryption (1-6): ";
+                << "\t1. Gronsfeld cipher\n\t2. Encryption using the square of Polybius\n\t3. Simple table permutation\n\t4. Atbash cipher\n\t5. Keyword cipher\n\t6. Table cipher with a keyword\n\t7. Caesar cipher\n\t8. Double table permutation\n\t9. 'Tarabarskaya gramota'\n\tAny other character: go back\nChoose encryption (1-9): ";
             getline(cin, cipher_str);
-            if (cipher_str == "1" or cipher_str == "2" or cipher_str == "3" or cipher_str == "4" or cipher_str == "5" or cipher_str == "6") cipher = cipher_str[0] - '0';
+            if (cipher_str == "1" or cipher_str == "2" or cipher_str == "3" or cipher_str == "4" or cipher_str == "5" or cipher_str == "6" or cipher_str == "7" or cipher_str == "8" or cipher_str == "9") cipher = cipher_str[0] - '0';
             else {
                 cout << "   [GO BACK]" << endl << endl;
                 continue;
@@ -505,6 +872,57 @@ int main() {
                 else cout << "    [Pincode is not correct!]" << endl;
                 break;
             }
+            case 7:
+            {
+                cout << endl << "    [Caesar]" << endl << endl;
+                if (pinAdmin == "0000") { // проверка того, задан ли пинкод
+                    cout << "    [Pincode not set!]" << endl;
+                    break;
+                }
+                string pinUser; // пинкод
+                cout << "Please enter the pincode: ";
+                getline(cin, pinUser); // ввод пинкода
+                if (checkPincode(pinUser, pinAdmin)) { // проверка того, совпадает ли заданный пинкод с вводимым
+                    cout << "    [Pincode validation passed successfully!]" << endl;
+                    Caesar();
+                }
+                else cout << "    [Pincode is not correct!]" << endl;
+                break;
+            }
+            case 8:
+            {
+                cout << endl << "    [Double table permutation]" << endl << endl;
+                if (pinAdmin == "0000") { // проверка того, задан ли пинкод
+                    cout << "    [Pincode not set!]" << endl;
+                    break;
+                }
+                string pinUser; // пинкод
+                cout << "Please enter the pincode: ";
+                getline(cin, pinUser); // ввод пинкода
+                if (checkPincode(pinUser, pinAdmin)) { // проверка того, совпадает ли заданный пинкод с вводимым
+                    cout << "    [Pincode validation passed successfully!]" << endl;
+                    DoubleTable();
+                }
+                else cout << "    [Pincode is not correct!]" << endl;
+                break;
+            }
+            case 9:
+            {
+                cout << endl << "    ['Tarabarskaya gramota']" << endl << endl;
+                if (pinAdmin == "0000") { // проверка того, задан ли пинкод
+                    cout << "    [Pincode not set!]" << endl;
+                    break;
+                }
+                string pinUser; // пинкод
+                cout << "Please enter the pincode: ";
+                getline(cin, pinUser); // ввод пинкода
+                if (checkPincode(pinUser, pinAdmin)) { // проверка того, совпадает ли заданный пинкод с вводимым
+                    cout << "    [Pincode validation passed successfully!]" << endl;
+                    Tarab();
+                }
+                else cout << "    [Pincode is not correct!]" << endl;
+                break;
+            }
             default:
                 break;
             }
@@ -514,6 +932,9 @@ int main() {
         else if (checkString("Atbash cipher: ")) antiatbash();
         else if (checkString("Keyword cipher: ")) antikeywordABC();
         else if (checkString("Table cipher with a keyword: ")) antitableCypher();
+        else if (checkString("Caesar: ")) antiCaesar();
+        else if (checkString("DoubleTable: ")) antiDoubleTable();
+        else if (checkString("Tarabarskaya: ")) antiTarab();
         }
         else if (dialogStr == "help") { // команда вызова списка команд
             cout << "    [COMMANDS]" << endl;
