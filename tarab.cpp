2f54869595e90213951ec6a45ee0dc048da9923d
daryashcherbinina <103048257+daryashcherbinina@users.noreply.github.com>
2022-06-12 16:33:48 +0700

    Update tarab.cpp

--- tarab.cpp
+++ tarab.cpp
@@ -18,51 +18,51 @@ void Tarab() {
         }
         if (word[i] >= 'a' && word[i] <= 'z')
             switch (word[i]) {
-                case 'b': word[i] = 'n'; break;
-                case 'c': word[i] = 'p'; break;
-                case 'd': word[i] = 'q'; break;
-                case 'f': word[i] = 'r'; break;
-                case 'g': word[i] = 's'; break;
-                case 'h': word[i] = 't'; break;
-                case 'j': word[i] = 'v'; break;
-                case 'k': word[i] = 'w'; break;
-                case 'l': word[i] = 'x'; break;
-                case 'm': word[i] = 'z'; break;
+                case 'b': word[i] = 'z'; break;
+                case 'c': word[i] = 'x'; break;
+                case 'd': word[i] = 'w'; break;
+                case 'f': word[i] = 'v'; break;
+                case 'g': word[i] = 't'; break;
+                case 'h': word[i] = 's'; break;
+                case 'j': word[i] = 'r'; break;
+                case 'k': word[i] = 'q'; break;
+                case 'l': word[i] = 'p'; break;
+                case 'm': word[i] = 'n'; break;
 
-                case 'n': word[i] = 'b'; break;
-                case 'p': word[i] = 'c'; break;
-                case 'q': word[i] = 'd'; break;
-                case 'r': word[i] = 'f'; break;
-                case 's': word[i] = 'g'; break;
-                case 't': word[i] = 'h'; break;
-                case 'v': word[i] = 'j'; break;
-                case 'w': word[i] = 'k'; break;
-                case 'x': word[i] = 'l'; break;
-                case 'z': word[i] = 'm'; break;
+                case 'n': word[i] = 'm'; break;
+                case 'p': word[i] = 'l'; break;
+                case 'q': word[i] = 'k'; break;
+                case 'r': word[i] = 'j'; break;
+                case 's': word[i] = 'h'; break;
+                case 't': word[i] = 'g'; break;
+                case 'v': word[i] = 'f'; break;
+                case 'w': word[i] = 'd'; break;
+                case 'x': word[i] = 'c'; break;
+                case 'z': word[i] = 'b'; break;
             }
         else
             switch (word[i]) {
-                case 'B': word[i] = 'N'; break;
-                case 'C': word[i] = 'P'; break;
-                case 'D': word[i] = 'Q'; break;
-                case 'F': word[i] = 'R'; break;
-                case 'G': word[i] = 'S'; break;
-                case 'H': word[i] = 'T'; break;
-                case 'J': word[i] = 'V'; break;
-                case 'K': word[i] = 'W'; break;
-                case 'L': word[i] = 'X'; break;
-                case 'M': word[i] = 'Z'; break;
+                case 'B': word[i] = 'Z'; break;
+                case 'C': word[i] = 'X'; break;
+                case 'D': word[i] = 'W'; break;
+                case 'F': word[i] = 'V'; break;
+                case 'G': word[i] = 'T'; break;
+                case 'H': word[i] = 'S'; break;
+                case 'J': word[i] = 'R'; break;
+                case 'K': word[i] = 'Q'; break;
+                case 'L': word[i] = 'P'; break;
+                case 'M': word[i] = 'N'; break;
 
-                case 'N': word[i] = 'B'; break;
-                case 'P': word[i] = 'C'; break;
-                case 'Q': word[i] = 'D'; break;
-                case 'R': word[i] = 'F'; break;
-                case 'S': word[i] = 'G'; break;
-                case 'T': word[i] = 'H'; break;
-                case 'V': word[i] = 'J'; break;
-                case 'W': word[i] = 'K'; break;
-                case 'X': word[i] = 'L'; break;
-                case 'Z': word[i] = 'M'; break;
+                case 'N': word[i] = 'M'; break;
+                case 'P': word[i] = 'L'; break;
+                case 'Q': word[i] = 'K'; break;
+                case 'R': word[i] = 'J'; break;
+                case 'S': word[i] = 'H'; break;
+                case 'T': word[i] = 'G'; break;
+                case 'V': word[i] = 'F'; break;
+                case 'W': word[i] = 'D'; break;
+                case 'X': word[i] = 'C'; break;
+                case 'Z': word[i] = 'B'; break;
             }
         stOut += word[i];
     }
@@ -79,51 +79,51 @@ void antiTarab() {
         }
         if (word[i] >= 'a' && word[i] <= 'z')
             switch (word[i]) {
-                case 'b': word[i] = 'n'; break;
-                case 'c': word[i] = 'p'; break;
-                case 'd': word[i] = 'q'; break;
-                case 'f': word[i] = 'r'; break;
-                case 'g': word[i] = 's'; break;
-                case 'h': word[i] = 't'; break;
-                case 'j': word[i] = 'v'; break;
-                case 'k': word[i] = 'w'; break;
-                case 'l': word[i] = 'x'; break;
-                case 'm': word[i] = 'z'; break;
+                case 'b': word[i] = 'z'; break;
+                case 'c': word[i] = 'x'; break;
+                case 'd': word[i] = 'w'; break;
+                case 'f': word[i] = 'v'; break;
+                case 'g': word[i] = 't'; break;
+                case 'h': word[i] = 's'; break;
+                case 'j': word[i] = 'r'; break;
+                case 'k': word[i] = 'q'; break;
+                case 'l': word[i] = 'p'; break;
+                case 'm': word[i] = 'n'; break;
 
-                case 'n': word[i] = 'b'; break;
-                case 'p': word[i] = 'c'; break;
-                case 'q': word[i] = 'd'; break;
-                case 'r': word[i] = 'f'; break;
-                case 's': word[i] = 'g'; break;
-                case 't': word[i] = 'h'; break;
-                case 'v': word[i] = 'j'; break;
-                case 'w': word[i] = 'k'; break;
-                case 'x': word[i] = 'l'; break;
-                case 'z': word[i] = 'm'; break;
+                case 'n': word[i] = 'm'; break;
+                case 'p': word[i] = 'l'; break;
+                case 'q': word[i] = 'k'; break;
+                case 'r': word[i] = 'j'; break;
+                case 's': word[i] = 'h'; break;
+                case 't': word[i] = 'g'; break;
+                case 'v': word[i] = 'f'; break;
+                case 'w': word[i] = 'd'; break;
+                case 'x': word[i] = 'c'; break;
+                case 'z': word[i] = 'b'; break;
             }
         else
             switch (word[i]) {
-                case 'B': word[i] = 'N'; break;
-                case 'C': word[i] = 'P'; break;
-                case 'D': word[i] = 'Q'; break;
-                case 'F': word[i] = 'R'; break;
-                case 'G': word[i] = 'S'; break;
-                case 'H': word[i] = 'T'; break;
-                case 'J': word[i] = 'V'; break;
-                case 'K': word[i] = 'W'; break;
-                case 'L': word[i] = 'X'; break;
-                case 'M': word[i] = 'Z'; break;
+                case 'B': word[i] = 'Z'; break;
+                case 'C': word[i] = 'X'; break;
+                case 'D': word[i] = 'W'; break;
+                case 'F': word[i] = 'V'; break;
+                case 'G': word[i] = 'T'; break;
+                case 'H': word[i] = 'S'; break;
+                case 'J': word[i] = 'R'; break;
+                case 'K': word[i] = 'Q'; break;
+                case 'L': word[i] = 'P'; break;
+                case 'M': word[i] = 'N'; break;
 
-                case 'N': word[i] = 'B'; break;
-                case 'P': word[i] = 'C'; break;
-                case 'Q': word[i] = 'D'; break;
-                case 'R': word[i] = 'F'; break;
-                case 'S': word[i] = 'G'; break;
-                case 'T': word[i] = 'H'; break;
-                case 'V': word[i] = 'J'; break;
-                case 'W': word[i] = 'K'; break;
-                case 'X': word[i] = 'L'; break;
-                case 'Z': word[i] = 'M'; break;
+                case 'N': word[i] = 'M'; break;
+                case 'P': word[i] = 'L'; break;
+                case 'Q': word[i] = 'K'; break;
+                case 'R': word[i] = 'J'; break;
+                case 'S': word[i] = 'H'; break;
+                case 'T': word[i] = 'G'; break;
+                case 'V': word[i] = 'F'; break;
+                case 'W': word[i] = 'D'; break;
+                case 'X': word[i] = 'C'; break;
+                case 'Z': word[i] = 'B'; break;
             }
         stOut += word[i];
     }
